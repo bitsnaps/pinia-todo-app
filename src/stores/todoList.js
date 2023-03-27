e893fdd1b39f37bf3d8225e110b03eae8fd71a56
Ibrahim.H <bitsnaps@yahoo.fr>
2023-03-27 16:32:16 +0100

    initial commit

---
+++ src/stores/todoList.js
@@ -0,0 +1,23 @@
+import { defineStore } from 'pinia'
+
+export const useTodoListStore = defineStore('todoList', {
+
+  // state
+  state: () => ({
+    todoList: [],
+    id: 0
+  }),
+
+  // actions
+  actions: {
+    addTodo(item) {
+      this.todoList.push({ item, id: this.id++, completed: false })
+    },
+    deleteTodo(itemId) {
+      this.todoList = this.todoList.filter((object) => {
+        return object.id !== itemId
+      })
+    }
+  }
+  // getters
+})
